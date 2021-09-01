e2db22adcd1ae1e6bec35b604f76207f6c1d7359
himani412 <77782996+himani412@users.noreply.github.com>
2021-09-01 07:11:56 -0700

    Add files via upload

---
+++ flowerpot.js
@@ -0,0 +1,23 @@
+function preload(){
+    img = loadImage('flowerpot.jpg');
+}
+function setup(){
+    canvas= createCanvas(600,400);
+    canvas.center();
+    objectDetecter = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects ";
+
+}
+function modelLoaded(){
+    console.log("ModelLoaded!");
+    status= true;
+    
+}
+function gotresult(error,results){
+    if(error){  
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+
+}
